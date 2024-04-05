6e51c2d40b79ad680a2ab0d6a98df92b66fe74f4
mattjburke <mattburke122@gmail.com>
2024-04-04 21:30:24 -0600

    update style

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+//function countLetters() {
+//    var input = document.getElementById('sentenceInput').value;
+//    var letterCount = input.replace(/[^a-zA-Z]/g, "").length; // Removes non-letters and counts
+//    document.getElementById('letterCount').innerText = letterCount;
+//}
+
+document.getElementById('letterForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the form from submitting in the traditional way
+    countLetters();
+});
+
 function countLetters() {
     var input = document.getElementById('sentenceInput').value;
-    var letterCount = input.replace(/[^a-zA-Z]/g, "").length; // Removes non-letters and counts
+    var letterCount = input.replace(/[^a-zA-Z]/g, "").length;
     document.getElementById('letterCount').innerText = letterCount;
 }
